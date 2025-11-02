b6cce6cf89437d4d5744740c22db8e6de916cc55
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-11-02 11:29:50 -0600

    ap working on basic configuration

--- main_node_code/esp32_ap_mode/main/ap_mode_config.c
+++ main_node_code/esp32_ap_mode/main/ap_mode_config.c
@@ -14,15 +14,16 @@
 static void wifi_event_handler(void *arg, esp_event_base_t base, int32_t id, void *data){
     switch(id){
         case WIFI_EVENT_AP_STACONNECTED:
-            wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t*) data;
-            printf("Station " MACSTR " connected\n", MAC2STR(event->mac));
+            wifi_event_ap_staconnected_t *connect_event = (wifi_event_ap_staconnected_t*) data;
+            printf("Station " MACSTR " connected\n", MAC2STR(connect_event->mac));
             break;
         case WIFI_EVENT_AP_STADISCONNECTED:
-            wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t*) data;
-            printf("Station " MACSTR " disconnected\n", MAC2STR(event->mac));
+            wifi_event_ap_stadisconnected_t *disconnect_event = (wifi_event_ap_stadisconnected_t*) data;
+            printf("Station " MACSTR " disconnected\n", MAC2STR(disconnect_event->mac));
             break;
         default:
             printf("event caught %s\n", base);
+            break;
     }
 }
 
@@ -36,4 +37,28 @@ void app_main(void){
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t init_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&init_config));
+
+
+    wifi_config_t ap_config = {
+        .ap = {
+            .ssid = "wi-formula-test",
+            .ssid_len = 0,
+            .channel = 1,
+            .password = "test_pass",
+            .max_connection = 4,
+            .authmode = WIFI_AUTH_WPA2_PSK
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
 }
