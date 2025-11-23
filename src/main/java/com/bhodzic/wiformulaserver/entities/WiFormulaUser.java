76d175ba9e73b0802a579c3cec63abcf38af17e1
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-11-22 19:27:20 -0600

    Refactor `WiFormulaUser` and `RegisteredDevice` entities by privatizing fields for better encapsulation.

--- src/main/java/com/bhodzic/wiformulaserver/entities/WiFormulaUser.java
+++ src/main/java/com/bhodzic/wiformulaserver/entities/WiFormulaUser.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class WiFormulaUser {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    public String user_id;
+    private String user_id;
     public String username;
     @JsonIgnore
     public String password;
