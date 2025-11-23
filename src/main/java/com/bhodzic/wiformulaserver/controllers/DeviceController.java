f2391c2157a27e150dedb4b3546c6dc6164fd6c5
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-11-22 19:24:20 -0600

    Add device approval API with `DeviceApprovalRequest` object in `DeviceController`

--- src/main/java/com/bhodzic/wiformulaserver/controllers/DeviceController.java
+++ src/main/java/com/bhodzic/wiformulaserver/controllers/DeviceController.java
@@ -4,6 +4,7 @@ import com.bhodzic.wiformulaserver.entities.RegisteredDevice;
 import com.bhodzic.wiformulaserver.entities.WiFormulaUser;
 import com.bhodzic.wiformulaserver.repositories.RegisteredDevicesRepo;
 import com.bhodzic.wiformulaserver.repositories.WiFormulaUserRepo;
+import com.bhodzic.wiformulaserver.request_objects.DeviceApprovalRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,5 +36,19 @@ public class DeviceController {
     public ResponseEntity<List<RegisteredDevice>> getRegisteredDevices(@PathVariable String user_id){
         Optional<WiFormulaUser> user = userRepo.findById(user_id);
         return user.map(wiFormulaUser -> new ResponseEntity<>(wiFormulaUser.registeredDevices, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    }
+    @PostMapping("/approve")
+    public ResponseEntity<RegisteredDevice> approveDevice(@RequestBody DeviceApprovalRequest device){
+        Optional<WiFormulaUser> user = userRepo.findById(device.user_id);
+        if(user.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<RegisteredDevice> deviceToApprove = deviceRepo.findById(device.mac_address);
+        if(deviceToApprove.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        deviceToApprove.get().user = user.get();
+        deviceRepo.save(deviceToApprove.get());
+        return new ResponseEntity<>(deviceToApprove.get(), HttpStatus.OK);
     }
 }
