2cd56d81fde351388c3295e566148a36c49f9b74
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-11-22 18:37:21 -0600

    Initialize Spring Boot project with Gradle setup, application structure, and configurations.

---
+++ src/main/java/com/bhodzic/wiformulaserver/WiFormulaServerApplication.java
@@ -0,0 +1,13 @@
+package com.bhodzic.wiformulaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiFormulaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiFormulaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhodzic/wiformulaserver/WiFormulaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhodzic.wiformulaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiFormulaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
