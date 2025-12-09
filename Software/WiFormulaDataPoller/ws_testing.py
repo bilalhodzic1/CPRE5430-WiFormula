0a69e2bc03be9507922fc7c2381507283dce9cd8
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-12-09 17:57:17 -0600

    added random in python poller

---
+++ Software/WiFormulaDataPoller/ws_testing.py
@@ -0,0 +1,32 @@
+import asyncio
+import websockets
+import random
+import json
+import time
+
+    # Generate a random integer between 1 and 10 (inclusive)
+async def connect_to_websocket(uri):
+    async with websockets.connect(uri) as ws:
+        print(f"Connected to {uri}")
+        while True:
+            random_int = random.randint(1, 100)
+            test_json = {
+                "random_int" : random_int
+            }
+            await ws.send(json.dumps(test_json))
+            time.sleep(5)
+        # # Receive messages
+        # while True:
+        #     try:
+        #         message = await ws.recv()
+        #         print(f"Received: {message}")
+        #     except websockets.exceptions.ConnectionClosedOK:
+        #         print("Connection closed by server.")
+        #         break
+        #     except Exception as e:
+        #         print(f"Error receiving message: {e}")
+        #         break
+
+if __name__ == "__main__":
+    websocket_uri = "ws://localhost:8080/formula-data-stream?device=PYTHON&mac=NOMAC"
+    asyncio.run(connect_to_websocket(websocket_uri))
